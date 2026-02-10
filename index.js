47dc131b20f535c00fea0a212887c8f4040b6a1c
Bautista <bautistamonroy6@gmail.com>
2026-02-10 02:11:17 -0300

    primer commit bot discord

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.Reaction
+  ]
+});
+
+// ===== CONFIGURACIÓN =====
+const CANAL_ORIGEN_ID = '1419319563178344468';
+const CANAL_DESTINO_ID = '1419808985552257085';
+const EMOJI = '✅';
+
+// ===== BOT LISTO =====
+client.once('ready', () => {
+  console.log(`Conectado como ${client.user.tag}`);
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (user.bot) return;
+
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch {
+      return;
+    }
+  }
+
+  const message = reaction.message;
+
+  // Validaciones
+  if (reaction.emoji.name !== EMOJI) return;
+  if (message.channel.id !== CANAL_ORIGEN_ID) return;
+
+  const canalDestino = await client.channels.fetch(CANAL_DESTINO_ID);
+  const autor = message.author;
+
+  await canalDestino.send(
+    `# 🎉 **¡Bienvenido/a al staff!** 🎉\n\n` +
+    `**${autor} fue aprobado y ahora forma parte del staff.\n**` +
+    `**¡Pasate por #guia-staff para orientacion! 💪**`
+  );
+});
+
+client.login(process.env.TOKEN);
